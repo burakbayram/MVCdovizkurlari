4eb3978ec3990efcd27a99abfbfbaf9709b771a8
burakbayram <bburak.bbayram@gmail.com>
2019-04-15 18:50:15 +0300

    Quartz

--- UserInterface/Global.asax.cs
+++ UserInterface/Global.asax.cs
@@ -12,7 +12,9 @@ using System.Web.Optimization;
 using System.Web.Routing;
 
 namespace UserInterface
-{
+{ /// <summary>
+/// Ouartz
+/// </summary>
     public class MvcApplication : System.Web.HttpApplication
     {
         protected void Application_Start()
